aab0546c62e1bd155fcb985d49fe5324bbbe95a4
pbateman828 <pbateman828@users.noreply.github.com>
2016-10-07 20:46:14 -0400

    Adding changes

---
+++ simple/learnpython.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3  
+import time 
+import praw  
+from pprint import pprint
+
+# r is the description that the reddit API sees. 
+#use your own username /u/ 
+r = praw.Reddit('PRAW related-question monitor by /u/#yourusername# v 1.1. '
+                'Url: https://praw.readthedocs.org/en/latest/'
+                'pages/writing_a_bot.html') 
+
+r.login() 
+
+submission = r.get_submission(submission_id = "105aru") 
+
+#vars contains the objects attributes and the values they contain
+
+pprint(vars(submission)) 
+
+#dir returns the names in the local scope
+
+pprint(dir(submission))
+
+
